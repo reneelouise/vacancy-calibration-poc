4ea1b70f83b86df92ee5da75d5eae37f39804335
Renee-Louise Nzegbulem <renee.nzegbulem@beamery.com>
2023-09-19 15:57:20 +0100

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is the start of the vacancy calibration insight POC hackathon!
+        </p>
+        <a
+          className="App-link"
+          href="https://docs.google.com/presentation/d/1PVaYNxmfhv-4SNxNFxeJNiii8B7cN6G_k1_REE_duAA/edit#slide=id.g280b56e0c08_0_935"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Take me to the slide deck
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
